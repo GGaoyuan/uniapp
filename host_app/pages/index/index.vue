81471da121cc1235a5723277d992d54974908911
gaoyuan <707216098@qq.com>
2024-03-04 09:58:46 +0800

    add

---
+++ host_app/pages/index/index.vue
@@ -0,0 +1,65 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<view class="newTextArea">
+			<!-- <text class="title">{{title}}</text> -->
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'aaaasdasdad',
+				text: 'text'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 0rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+		background-color: aqua;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #800f94;
+	}
+	
+	.newTextArea {
+		display: flex;
+		justify-content: center;
+		background-color: aqua;
+		height: 80rpx;
+		width: 100rpx;
+	}
+</style>
