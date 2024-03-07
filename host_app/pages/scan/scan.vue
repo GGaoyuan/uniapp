b880e314a0914c9a256cf26ccdddcba30ea43a35
gaoyuan <707216098@qq.com>
2024-03-07 13:41:50 +0800

    add

--- host_app/pages/scan/scan.vue
+++ host_app/pages/scan/scan.vue
@@ -1,114 +1,145 @@
 <template>
-	<view class="container-area">
-		<view class="top">A</view>
-		<!-- <view class="middle">B</view> -->
-		<uni-list class="middle">
-			<text>BBBBBB</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>AAAAAA</text>
-			<text>CCCCCC</text>
-		</uni-list>
-		<view class="bottom">C</view>
+	<view class="container">
+		<view class="header">
+			<view class="" style="width: 100%;height: 80rpx;">
+				Header
+			</view>
+		</view> 
+		<scroll-view class="content" scroll-y>
+			<view class="" style="padding: 0 20rpx;">
+				<view class="list-item" v-for="item in list" :key="item.id">{{ item.text }}</view>
+				<view class="list-item" v-for="item in list" :key="item.id">{{ item.text }}</view>
+			</view>
+		</scroll-view>
+		<view class="footer">
+			<view class="" style="width: 100%;height: 80rpx;"> Footer </view>
+		</view>
 	</view>
-
 </template>
-
-
 
 <script>
 	export default {
-		components: {},
 		data() {
 			return {
-
+				list: [{
+						id: 1,
+						text: 'Item 1'
+					},
+					{
+						id: 2,
+						text: 'Item 2'
+					},
+					{
+						id: 3,
+						text: 'Item 3'
+					},
+					{
+						id: 4,
+						text: 'Item 4'
+					},
+					{
+						id: 5,
+						text: 'Item 5'
+					},
+					{
+						id: 6,
+						text: 'Item 6'
+					},
+					{
+						id: 7,
+						text: 'Item 7'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 8,
+						text: 'Item 8'
+					},
+					{
+						id: 9,
+						text: 'Item 9'
+					}
+				],
 			}
-		},
-		onLoad() {
-
-		},
-		onReady() {
-
-		},
-		methods: {
-
 		},
 	}
 </script>
 
-
-
-<style lang="scss">
-	.container-area {
+<style lang="scss" scoped>
+	.container {
 		display: flex;
 		flex-direction: column;
-		position: absolute;
-		top: 0rpx;
-		left: 0rpx;
-		right: 0rpx;
-		bottom: 0rpx;
-		background-color: blue;
+		height: 100vh;
 	}
 
-	.top {
+	.header {
 		position: sticky;
-		top: 0rpx;
-		min-height: 100rpx;
-		background-color: red;
-		z-index: 100;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: #333;
+		color: #fff;
+		height: auto;
+		text-align: center;
 	}
 
-	.middle {
-		flex-grow: 1;
-		margin-top: 0rpx;
-		margin-bottom: 0rpx;
-		background-color: green;
-		z-index: 1;
+	.content {
+		flex: 1;
+		overflow: auto;
 	}
 
-	.bottom {
+	.list-item {
+		height: 80px;
+		line-height: 80px;
+		border: 1px solid red;
+		text-align: center;
+	}
+
+	.footer {
 		position: sticky;
-		bottom: 0rpx;
-		min-height: 100rpx;
-		background-color: yellow;
-		z-index: 100;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		background-color: #333;
+		color: #fff;
+		text-align: center;
 	}
 </style>
