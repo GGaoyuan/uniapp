99a082a43d6f7371452f9afe5164fa9743ea770e
gaoyuan <707216098@qq.com>
2024-03-14 20:37:24 +0800

    add

---
+++ uni-users/src/static/data/testjson.js
@@ -0,0 +1,99 @@
+const reviewData = 
+{
+    "list": [
+        {
+            "motoType": "风车车",
+            "status": 0,
+            "motoColor": "黑色",
+            "motoBrand": "雅迪",
+            "motoNumber": "AAA00000000011111111111111111111",
+            "motoRFID": "BBB00000000000"
+        },
+		{
+		    "motoType": "风车车",
+		    "status": 1,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA111111111111",
+		    "motoRFID": "BBB111111111111"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 2,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA222222222222222222",
+		    "motoRFID": "BBB222222222222222222"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 3,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 4,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 3,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 4,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 3,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		},
+		{
+		    "motoType": "风车车",
+		    "status": 4,
+		    "motoColor": "黑色",
+		    "motoBrand": "雅迪",
+		    "motoNumber": "AAA33333333333333333",
+		    "motoRFID": "BBB3333333333333333333333"
+		}
+    ]
+}
+
+
+const banner = [{
+		id: 0,
+		src: '../../static/img/banner0.png'
+	},
+	{
+		id: 1,
+		src: '../../static/img/banner1.png'
+	},
+	{
+		id: 2,
+		src: '../../static/img/banner2.png'
+	},
+]
+
+// 定义数据出口
+
+module.exports = {
+	reviewData: reviewData,
+	banner: banner
+}
---
+++ user_app/unpackage/dist/dev/mp-weixin/pages/regist/components/regist-carinfo.js
@@ -0,0 +1,15 @@
+"use strict";
+const common_vendor = require("../../../common/vendor.js");
+const _sfc_main = /* @__PURE__ */ common_vendor.defineComponent({
+  __name: "regist-carinfo",
+  props: {
+    title: String
+  },
+  setup(__props) {
+    return (_ctx, _cache) => {
+      return {};
+    };
+  }
+});
+const Component = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-7b81d94a"], ["__file", "/Users/zz/HBuilderX/user_app/pages/regist/components/regist-carinfo.vue"]]);
+wx.createComponent(Component);
